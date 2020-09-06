dd3945c66b1b51530d98dde0ea13a05d98475f51
susom <susomshrestha@gmail.com>
2020-09-06 15:04:02 +0545

    add popper, bootstrap and jquery

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './pages/main/main.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { MainRoutingModule } from './main.routing';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [MainComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    MainRoutingModule,
+    RouterModule
   ]
 })
 export class MainModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NavBarComponent, FooterComponent],
+  exports: [
+    NavBarComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
-  ]
+  ],
 })
 export class SharedModule { }
